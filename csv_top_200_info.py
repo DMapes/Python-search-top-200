17f33c4848e51da523665c62e678dabf4b26aa52
Dan Mapes <daniel.r.mapes@gmail.com>
2019-08-28 22:51:45 -0700

    Update search options
    Search for top position payers.

--- csv_top_200_info.py
+++ csv_top_200_info.py
@@ -70,22 +70,56 @@ def player_details(players):
         player_details_list.append(info)
     return player_details_list
 
+def position_details(pos, players):
+    position_details_list = []
+    for name in players:
+        rank, player_name, position, team, bye, player_info = search_player(name)
+        if position == pos:
+            info = '{} - {}-{}-{} Bye|{}'.format(rank, player_name, position, team, bye)
+            position_details_list.append(info)
+    return position_details_list[:5]
+
 # print player_info_list
 # print player_chosen
 
 # add, remove, new, close = raw_input('add, remove, new, close').split()
-print available_player_list
+print 'Top 200 Players: {}'.format(available_player_list)
 
 my_team = []
 
 input_name = True
 
 while input_name:
-    print 'Top 5 available {}'.format(player_details(available_player_list[:5]))
-    input_name = raw_input('(name)Search Player. (t)show team. : ')
+    # print 'Top 5 available{}'.format(player_details(available_player_list[:5]))
+    input_name = raw_input('Search: ')
     try:
+        if input_name == 'o':
+            print '(5)Top 5 Available. (t)Show team. (qb)Top 5 QB. (rb)Top 5 RB. (wr)Top 5 WR. (te)Top 5 TE. \
+            (d)Top 5 DEF. (k) top 5 K.'
+            continue
+        if input_name == '5':
+            print 'Top 5 available{}'.format(player_details(available_player_list[:5]))
+            continue
         if input_name == 't':
             print 'My Team : {}'.format(player_details(my_team))
+            continue
+        if input_name == 'qb':
+            print 'Top 5 QB{}'.format(position_details('QB', available_player_list))
+            continue
+        if input_name == 'rb':
+            print 'Top 5 RB{}'.format(position_details('RB', available_player_list))
+            continue
+        if input_name == 'wr':
+            print 'Top 5 WR{}'.format(position_details('WR', available_player_list))
+            continue
+        if input_name == 'te':
+            print 'Top 5 TE{}'.format(position_details('TE', available_player_list))
+            continue
+        if input_name == 'd':
+            print 'Top 5 DEF{}'.format(position_details('DEF', available_player_list))
+            continue
+        if input_name == 'k':
+            print 'Top 5 K{}'.format(position_details('PK', available_player_list))
             continue
         rank, player_name, position, team, bye, player_info = search_player(input_name)
         print 'Rank|{} Player|{} Team|{} Position|{} Bye|{}'.format(rank, player_name, position, team, bye)
@@ -103,13 +137,13 @@ while input_name:
             continue
         if player_name not in available_player_list:
             print '{} is not available.'.format(player_name)
-            options = raw_input('(r)remove from team, (a)add to available. (c) continue')
+            options = raw_input('(r)remove from team, (a)add to available.')
             if options == 'r':
                 remove_team_player(player_name)
                 print '{} removed from team.'.format(player_name)
             if options == 'a':
                 available_player_list.append(player_name)
-            if options == False:
+            if options is False:
                 continue
     except:
         print 'no match try again'
@@ -117,7 +151,7 @@ while input_name:
 else:
     print 'My Team : {}'.format(player_details(my_team))
 
-print 'Still avaliable {}'.format(available_player_list)
+print 'Still available {}'.format(available_player_list)
 
 # path = os.path.expandvars('{0}\{1}.csv'.format(new_folder,schedule_name))
 # csvfile = open(path, 'w')
